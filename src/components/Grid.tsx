c1737775520b292e63fee47c2be498419e5df92e
Roman Dyadyushkin <rd.rd.2250@gmail.com>
2022-02-03 18:38:09 +0200

    Add Grid component

---
+++ src/components/Grid.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+import './Grid.scss';
+
+type GridProps = {
+  rows: number;
+  cols: number;
+};
+
+export const Grid: FC<GridProps> = ({ rows, cols }): JSX.Element => {
+  return (
+    <div className='grid'>
+      {Array(rows * cols)
+        .fill(0)
+        .map((elem, idx) => {
+          return <div key={`${idx}`} className={`grid__cell`} />;
+        })}
+    </div>
+  );
+};
