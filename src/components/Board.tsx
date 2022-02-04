48aa36b364ff9221002ddc284d63e904d7fd6e1c
Roman Dyadyushkin <rd.rd.2250@gmail.com>
2022-02-04 14:22:06 +0200

    Add Tiles to Board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,12 +1,16 @@
 import { FC } from 'react';
+import { TileMeta } from '../types';
 import { Grid } from './Grid';
+import { Tile } from './Tile';
+import './Board.scss';
 
 type BoardProps = {
   cols: number;
   rows: number;
+  tiles: TileMeta[];
 };
 
-export const Board: FC<BoardProps> = ({ cols, rows }): JSX.Element => {
+export const Board: FC<BoardProps> = ({ cols, rows, tiles }): JSX.Element => {
   const tileTotalWidth = 100;
   const boardPadding = 10;
 
@@ -16,6 +20,9 @@ export const Board: FC<BoardProps> = ({ cols, rows }): JSX.Element => {
 
   return (
     <div className='board' style={{ width: boardWidth }}>
+      {tiles.map((tile) => (
+        <Tile key={tile.id} value={tile.value} position={tile.position} />
+      ))}
       <Grid rows={rows} cols={cols} />
     </div>
   );
