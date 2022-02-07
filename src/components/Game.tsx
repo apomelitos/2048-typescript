73815dc3ab2b6c629ad1e96a3118ae5ede695a0d
Roman Dyadyushkin <rd.rd.2250@gmail.com>
2022-02-07 17:03:32 +0200

    Fix Board component props

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,13 +1,16 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import { TileMeta } from '../types';
 import { Board } from './Board';
 
 const ROWS = 4;
 const COLS = 4;
 
 export const Game: FC = (): JSX.Element => {
+  const [state, setState] = useState<TileMeta[]>([]);
+
   return (
     <div>
-      <Board cols={COLS} rows={ROWS} />
+      <Board cols={COLS} rows={ROWS} tiles={state} />
     </div>
   );
 };
