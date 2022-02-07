242abbd1e8c090d3c2c8ffe859907c2053d1076a
Roman Dyadyushkin <rd.rd.2250@gmail.com>
2022-02-07 17:19:18 +0200

    Remove Board component

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,16 +1,37 @@
 import { FC, useState } from 'react';
 import { TileMeta } from '../types';
-import { Board } from './Board';
+import { Tile } from './Tile';
+import { Grid } from './Grid';
 
 const ROWS = 4;
 const COLS = 4;
+const TILE_TOTAL_WIDTH = 100;
+const BOARD_PADDING = 10;
+const CONTAINER_WIDTH = TILE_TOTAL_WIDTH * COLS;
+const BOARD_WIDTH = CONTAINER_WIDTH + BOARD_PADDING * 2;
+
+const initialState: TileMeta[] = [
+  {
+    id: 1,
+    value: 2,
+    position: [0, 0],
+  },
+  {
+    id: 2,
+    value: 4,
+    position: [0, 1],
+  },
+];
 
 export const Game: FC = (): JSX.Element => {
-  const [state] = useState<TileMeta[]>([]);
+  const [tiles] = useState<TileMeta[]>(initialState);
 
   return (
-    <div>
-      <Board cols={COLS} rows={ROWS} tiles={state} />
+    <div className='board' style={{ width: BOARD_WIDTH, position: 'relative' }}>
+      {tiles.map((tile) => (
+        <Tile key={tile.id} value={tile.value} position={tile.position} />
+      ))}
+      <Grid rows={ROWS} cols={COLS} />
     </div>
   );
 };
