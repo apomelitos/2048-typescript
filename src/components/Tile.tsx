9389042ed812bd2c28c53e68e836c23c9c6e17e2
Roman Dyadyushkin <rd.rd.2250@gmail.com>
2022-02-07 15:12:20 +0200

    Add Tile component

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,4 +1,5 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
+import { usePrevValue } from '../hooks/usePrevValue';
 import './Tile.scss';
 
 type TileProps = {
@@ -7,23 +8,50 @@ type TileProps = {
 };
 
 type TopLeftStyles = {
-  top: number;
-  left: number;
+  top: string;
+  left: string;
 };
 
 const positionToPixels = ([row, col]: number[]): TopLeftStyles => {
   return {
-    top: row * 100 + 10,
-    left: col * 100 + 10,
+    // 100 is tile size with margins
+    top: `${col * 100 + 10}px`,
+    left: `${row * 100 + 10}px`,
   };
 };
 
 export const Tile: FC<TileProps> = ({ value, position }): JSX.Element => {
   const [tileValue, setTileValue] = useState(value);
+  const tileRef = useRef<HTMLDivElement | null>(null);
+  const prevValue = usePrevValue(tileValue);
+
+  const shallAnimate = prevValue !== tileValue;
+
+  useEffect(() => {
+    if (!tileRef.current) return;
+
+    tileRef.current.animate([positionToPixels(position)], {
+      duration: 300,
+      fill: 'forwards',
+    });
+
+    if (shallAnimate) {
+      tileRef.current.animate(
+        [
+          { transform: 'scale(1) rotate(0deg)' },
+          { transform: 'scale(1.2) rotate(180deg)' },
+          { transform: 'scale(1) rotate(0deg)' },
+        ],
+        {
+          duration: 300,
+        }
+      );
+    }
+  }, [position, shallAnimate]);
 
   return (
-    <div className={`tile tile-${tileValue}`} style={positionToPixels(position)}>
-      {tileValue}
+    <div className={`tile tile-${tileValue}`} ref={tileRef} style={{ zIndex: tileValue }}>
+      {prevValue || tileValue}
     </div>
   );
 };
