8b8329451b211c2df794a54dd8527d8029da3085
Roman Dyadyushkin <rd.rd.2250@gmail.com>
2022-02-09 12:37:05 +0200

    Add creating new tile if state has changed

--- src/hooks/useHandleButtons.ts
+++ src/hooks/useHandleButtons.ts
@@ -19,6 +19,29 @@ const getTilesMatrix = (tiles: TileMeta[]): TileMeta[][] => {
 
 const isTile = (obj: unknown): obj is TileMeta => !!obj && typeof obj === 'object' && 'position' in obj;
 
+const generateRandomTile = (tiles: TileMeta[]) => {
+  const matrix = getTilesMatrix(tiles);
+  const emptyCellsPositions: [number, number][] = [];
+
+  for (let rowIdx = 0; rowIdx < SIZE; rowIdx++) {
+    for (let colIdx = 0; colIdx < SIZE; colIdx++) {
+      console.log(`Iteration: ${rowIdx * matrix.length + colIdx}`);
+      if (!isTile(matrix[rowIdx][colIdx])) {
+        emptyCellsPositions.push([rowIdx, colIdx]);
+      }
+    }
+  }
+
+  const position = emptyCellsPositions[Math.floor(Math.random() * emptyCellsPositions.length)];
+  console.log(emptyCellsPositions);
+
+  return {
+    id: Date.now(),
+    value: 2,
+    position,
+  };
+};
+
 const updateState = (state: TileMeta[], direction: 'UP' | 'DOWN' | 'LEFT' | 'RIGHT'): TileMeta[] => {
   const matrix = getTilesMatrix(state);
   const newState: TileMeta[] = [];
@@ -33,6 +56,7 @@ const updateState = (state: TileMeta[], direction: 'UP' | 'DOWN' | 'LEFT' | 'RIG
   let getNextPosition = getNextPositionInRow;
   let pointerStart = 0;
   let firstIdx = 0;
+  let changesCount = 0;
 
   if (direction === 'UP' || direction === 'DOWN') {
     getTile = getTileFromCol;
@@ -70,11 +94,25 @@ const updateState = (state: TileMeta[], direction: 'UP' | 'DOWN' | 'LEFT' | 'RIG
         const position = getNextPosition(pointer, firstIdx);
         newState.push({ ...tile, position, value });
         pointer += pointerStep;
+
+        if (position[0] !== tile.position[0] || position[1] !== tile.position[1]) {
+          changesCount++;
+        }
       }
     }
   }
 
-  return newState.filter((tile) => !toDeleteTilesIDs.includes(tile.id));
+  // console.log(changesCount);
+
+  const mergedState = newState.filter((tile) => !toDeleteTilesIDs.includes(tile.id));
+
+  if (changesCount > 0) {
+    const newTile = generateRandomTile(mergedState);
+    console.log('New tile', newTile);
+    return [...mergedState, newTile];
+  }
+
+  return mergedState;
 };
 
 export const useHandleButtons = (setState: React.Dispatch<SetStateAction<TileMeta[]>>) => {
